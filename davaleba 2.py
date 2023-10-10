0aa08bfaa1fc6705a6e12c7b64a48be0da04720a
tamtiko <147196741+tamtiko@users.noreply.github.com>
2023-10-10 17:30:32 +0400

    Update davaleba 2.py

--- davaleba 2.py
+++ davaleba 2.py
@@ -2,6 +2,6 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return n*factorial(n-1)
-n=25
-print(factortial(n))
+        return n * factorial(n-1)
+n = 25
+print(factorial(n))
